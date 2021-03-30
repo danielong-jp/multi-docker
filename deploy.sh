2387f20f5ebda37774a67d33d8693e2bddc02b4e
Daniel Ong <crimsonknightd@hotmail.com>
2021-03-30 13:45:51 +0900

    add deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+# build images 
+docker build -t crimsonknightd/multi-client:latest -t crimsonknightd/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t crimsonknightd/multi-server:latest -t crimsonknightd/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t crimsonknightd/multi-worker:latest -t crimsonknightd/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+# push latest docker images
+docker push crimsonknightd/multi-client:latest
+docker push crimsonknightd/multi-server:latest
+docker push crimsonknightd/multi-worker:latest
+
+# push sha docker images
+docker push crimsonknightd/multi-client:$SHA
+docker push crimsonknightd/multi-server:$SHA
+docker push crimsonknightd/multi-worker:$SHA
+
+# apply kubernetes
+kubectl apply -f ./k8s
+kubectl set image deplyoments/server-deployment server=crimsonknightd/multi-server:$SHA
+kubectl set image deployments/client-deployment client=crimsonknightd/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=crimsonknightd/multi-worker:$SHA
